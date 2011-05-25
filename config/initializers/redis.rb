bf59d8120d9695f263e1d557ab7faea24a8b9009
Manu Raghavan <manu.raghavan@gmail.com>
2011-05-25 11:37:56 -0400

    Redis settings

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
 require 'redis'
-require 'redis/text_search'
-Redis::TextSearch.redis = Redis.new(:host => 'localhost', :port => 6379)
-#$redis = Redis::TextSearch.redis
+#Redis::TextSearch.redis = Redis.new(:host => 'localhost', :port => 6379)
+$redis = Redis.new(:host => 'localhost', :port => 6379)
+
