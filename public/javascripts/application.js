03aaf0266c86b27d3a511a8fd041aa35042e3912
Manu Raghavan <manu.raghavan@gmail.com>
2011-05-25 15:47:59 -0400

    Ajax-y search

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(function() {
+  $("#records_search").submit(function() {
+    $.get(this.action, $(this).serialize(), null, "script")
+    return false
+  });
+});
